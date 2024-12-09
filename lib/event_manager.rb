5f0ddde80e1da0683f5856e5448e4d4a86e6b707
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-09 16:31:17 +0100

    Import secret.key

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+civic_info.key = File.read('secret.key').strip
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -31,6 +31,6 @@ contents.each do |row|
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
-  
+
   puts "#{name} #{zipcode} #{legislators}"
 end
