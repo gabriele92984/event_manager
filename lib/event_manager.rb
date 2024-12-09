68aa05ece3be6ddf89a3f73606a749299fb17d05
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-09 15:57:37 +0100

    Installing the Google API Client

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,32 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'EventManager initialized.'
 
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
